b7c8fd1653d1ef9ed5d5238f3f0e4711f8735af9
David Teply <davehamdan@gmail.com>
2021-09-19 14:50:24 -0700

    adding publish.cmd

---
+++ publish.cmd
@@ -0,0 +1,13 @@
+@echo off
+echo Current gh-pages:
+echo.
+git ls-tree --name-only -r gh-pages
+echo.
+
+set /p version="Enter version(e.g. 0.2): "
+echo %version%
+
+@echo on
+ng build --prod --output-path demo-%version% --baseHref="https://daveteply.github.io/webgl-v0/demo-%version%/"
+
+@echo off
